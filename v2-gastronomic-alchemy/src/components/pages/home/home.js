861df5fac5b33e7071dc91a11d00a4311f2fee65
njenkins2727 <njenkins2727@gmail.com>
2024-02-07 10:26:17 +0800

    home component complete, working on cards next to add to home component

--- v2-gastronomic-alchemy/src/components/pages/home/home.js
+++ v2-gastronomic-alchemy/src/components/pages/home/home.js
@@ -1,10 +1,17 @@
-
+import hero from '../../images/img-hero.png'
+import './home.css'
 function Home(){
     return(
-        <div class="main">
-        Hellow 
+        <div className="body">
+            <img src={hero} className="hero"/>
+        <div class="hero-text">
+            <h1>make.food.delicious</h1>
+            <p>your new go to recipe website</p>
+            <button className='btn btn-primary'>view recipes</button>
         </div>
-    )
+        {/* cards here */}
+        </div>
+    ) 
 }
 
 export default Home;
